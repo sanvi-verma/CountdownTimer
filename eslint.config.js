6bce9efcb52ffe400a0f36f391bf1456cb65b69f
Sanvi Verma <137278991+sanvi-verma@users.noreply.github.com>
2025-03-07 10:56:00 +0530

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+    {
+      ignores: ["node_modules/"],
+    },
+    {
+      languageOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "off",
+      },
+    },
+  ];
+  
